3e8085fd0015bc04193b8a86d2e3afc5b6fb1844
mklarrieu <mklarrieu@gmail.com>
2019-09-14 19:50:14 -0400

    Finished project

--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
-//#includes are ALWAYS at top of file
-#include <iostream>	//iostream is where std::cout and std::endl are defined
-
-std::string func()
-{
-	return "hello";
-}
+#include "myfunc.h"//#includes are ALWAYS at top of file
 
 int main()
 {
